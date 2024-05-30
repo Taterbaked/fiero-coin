90059530379cf99cf4b82d664ad35c1ddefc3d07
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2024-05-30 15:01:05 -0400

    p2p: Deal with shortid collisions for reconciliation sets
    If a wtxid to be added to a peer's recon set has a shot id collisions (a previously
    added wtxid maps to the same short id), both transaction should be fanout, given
    our peer may have added the opposite transaction to our recon set, and reconciliation
    will fail. Moreover, all descendants of the previously added transaction that were in
    the recon set should also be removed and fanout.

--- src/node/txreconciliation.h
+++ src/node/txreconciliation.h
@@ -91,6 +91,12 @@ public:
      */
     uint64_t PreRegisterPeer(NodeId peer_id);
 
+
+    /**
+     * For testing purposes only. This SHOULD NEVER be used in production.
+    */
+    void PreRegisterPeerWithSalt(NodeId peer_id, uint64_t local_salt);
+
     /**
      * Step 0. Once the peer agreed to reconcile txs with us, generate the state required to track
      * ongoing reconciliations. Must be called only after pre-registering the peer and only once.
@@ -99,11 +105,16 @@ public:
                                               uint32_t peer_recon_version, uint64_t remote_salt);
 
     /**
-     * Step 1. Add a new transaction we want to announce to the peer to the local reconciliation set
-     * of the peer, so that it will be reconciled later, unless the set limit is reached.
-     * Returns whether the transaction appears in the set.
+     * Step 1. Add a to-be-announced transaction to the local reconciliation set of the target peer.
+     * Returns false if the set is at capacity, or if the set contains a colliding transaction (alongside
+     * the colliding wtxid). Returns true if the transaction is added to the set (or if it was already in it).
      */
     AddToSetResult AddToSet(NodeId peer_id, const Wtxid& wtxid);
+
+    /**
+     * Checks whether a wtxid has a short id collision in the peer's reconciliation set.
+    */
+   bool HasCollision(NodeId peer_id, const Wtxid& wtxid, Wtxid& collision, uint32_t &short_id);
 
     /**
      * Before Step 2, we might want to remove a wtxid from the reconciliation set, for example if
