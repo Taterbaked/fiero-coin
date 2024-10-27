sdaftuar/bitcoin

Switch to using txgraph parents/children in public accessors

src/txmempool.cpp
@@ -51,8 +51,8 @@ bool TestLockPointValidity(CChain& active_chain, const LockPoints& lp)
 std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> CTxMemPool::GetChildren(const CTxMemPoolEntry& entry) const
 {
     std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> ret;
-    for (auto child : entry.GetMemPoolChildrenConst()) {
-        ret.push_back(child);
+    for (auto child : entry.GetTxEntryChildren()) {
+        ret.push_back(static_cast<const CTxMemPoolEntry &>(child.get()));
     }
     return ret;
 }
@@ -60,8 +60,8 @@ std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> CTxMemPool::GetChildren(const C
 std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> CTxMemPool::GetParents(const CTxMemPoolEntry& entry) const
 {
     std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> ret;
-    for (auto child : entry.GetMemPoolParentsConst()) {
-        ret.push_back(child);
+    for (auto parent : entry.GetTxEntryParents()) {
+        ret.push_back(static_cast<const CTxMemPoolEntry &>(parent.get()));
     }
     return ret;
 }

Taterbaked/fiero-coin

src/txmempool.cpp
@@ -54,8 +54,8 @@ bool TestLockPointValidity(CChain& active_chain, const LockPoints& lp)
 std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> CTxMemPool::GetChildren(const CTxMemPoolEntry& entry) const
 {
     std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> ret;
-    for (auto child : entry.GetMemPoolChildrenConst()) {
-        ret.push_back(child);
+    for (auto child : entry.GetTxEntryChildren()) {
+        ret.emplace_back(static_cast<const CTxMemPoolEntry &>(child.get()));
     }
     return ret;
 }
@@ -63,8 +63,8 @@ std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> CTxMemPool::GetChildren(const C
 std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> CTxMemPool::GetParents(const CTxMemPoolEntry& entry) const
 {
     std::vector<CTxMemPoolEntry::CTxMemPoolEntryRef> ret;
-    for (auto child : entry.GetMemPoolParentsConst()) {
-        ret.push_back(child);
+    for (auto parent : entry.GetTxEntryParents()) {
+        ret.emplace_back(static_cast<const CTxMemPoolEntry &>(parent.get()));
     }
     return ret;
 }